f105c07ba99743fedc8a7282a978418de5453ba0
Wilson Gurung <wilsongurung12@gmail.com>
2019-07-21 15:29:10 +0545

    First Version

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+    <div class="row">
+    <div class="col-sm-6 offset-3">
+    <b-nav tabs>
+        <b-nav-item disabled><strong>Fancy Quiz App</strong></b-nav-item>
+        <b-nav-item disabled class="float-right"><strong>Counter: </strong> {{ numCorrect }}/{{ numTotal }}</b-nav-item>
+    </b-nav>
+    </div>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'numCorrect',
+        'numTotal'
+    ]
+}
+</script>
+
